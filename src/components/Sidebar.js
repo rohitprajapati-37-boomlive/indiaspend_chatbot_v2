31199c89d774ce249db24e2a19c2997dc2ef7f30
rohitprajapati-37-boomlive <163284552+rohitprajapati-37-boomlive@users.noreply.github.com>
2025-03-14 10:35:40 +0530

    mobile menu fixed
    mobile menu design and function fixed

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -7,6 +7,9 @@ import React, { useState, useEffect } from "react";
 // import { GoSidebarCollapse } from "react-icons/tb";
 
 // import { GoCrossReference } from "react-icons/fi";
+
+import { FiMenu, FiX } from "react-icons/fi";
+
 import { BsGenderTrans } from "react-icons/bs";
 import {
   GoQuestion,
@@ -36,6 +39,8 @@ function Sidebar({
   // const [isCollapsed, setIsCollapsed] = useState(false);
   const [previousQuestions, setPreviousQuestions] = useState([]);
 
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false); // ✅ Mobile menu toggle ke liye state
+
   useEffect(() => {
     const storedHistory =
       JSON.parse(localStorage.getItem("questionHistory")) || [];
@@ -59,126 +64,155 @@ function Sidebar({
   };
 
   return (
-    <div class="sidebar_abh">
-      <div class="logoDiv">
+    <>
+      {/* ☰ Menu Button (Left Side) */}
+      {!isMobileMenuOpen && (
         <div
-          className="logo-container"
-          onClick={() => window.location.reload()}
+          className="mobile-menu-icon left"
+          onClick={() => setIsMobileMenuOpen(true)}
         >
-          <img src={logo} alt="IndiaSpend Logo" className="logo" />
-          {isCollapsed && <h4>Ask IndiaSpend</h4>}
+          <FiMenu />
         </div>
-      </div>
+      )}
 
-      <div class="listbox">
-        {/* <div className="sidebar-content"> */}
-        <div className="sidebar_block">
-          {/* Sidebar Toggle Button */}
-          <p className="toggle-button sidebar-icon" onClick={toggleSidebar}>
-            {isCollapsed ? <GoSidebarCollapse /> : <GoSidebarExpand />}
-            <span className={`sidebar-text ${isCollapsed ? "" : "hidden"}`}>
-              Collapse
-            </span>
-          </p>
+      {/* Sidebar */}
+      <div className={`sidebar_abh ${isMobileMenuOpen ? "open" : ""}`}>
+        {/* ✖ Close Button (Right Side) */}
+        {isMobileMenuOpen && (
+          <div
+            className="mobile-menu-icon right"
+            onClick={() => setIsMobileMenuOpen(false)}
+          >
+            <FiX />
+          </div>
+        )}
 
-          <p className="new-thread-button" onClick={handleNewThread}>
-            <span className="sidebar-icon">
-              <GoCrossReference />
-            </span>
-            <span className={`sidebar-text ${isCollapsed ? "" : "hidden"}`}>
-              Start new thread
-            </span>
-          </p>
+        <div class="logoDiv">
+          <div
+            className="logo-container"
+            onClick={() => window.location.reload()}
+          >
+            <img src={logo} alt="IndiaSpend Logo" className="logo" />
+            {isCollapsed && <h4>Ask IndiaSpend</h4>}
+          </div>
         </div>
-        {/* Quick Links */}
-        <div className="sidebar_block">
-          <div className={`ttlquicklink ${isCollapsed ? "" : "hidden"}`}>
-            {isCollapsed && <h5> Quick Links</h5>}
 
-            {[
-              {
-                text: "IndiaSpend",
-                link: "https://www.indiaspend.com",
-                icon: <GoLink />,
-              },
-              {
-                text: "Earthcheck India",
-                link: "https://www.indiaspend.com/earthcheckindia",
-                icon: <GoGlobe />,
-              },
-              {
-                text: "Education Check",
-                link: "https://www.indiaspend.com/education-check",
-                icon: <GoRepo />,
-              },
-              {
-                text: "GenderCheck",
-                link: "https://www.indiaspend.com/gendercheck",
-                icon: <BsGenderTrans />,
-              },
-              {
-                text: "Newsletters",
-                link: "https://www.indiaspend.com/subscribe",
-                icon: <GoMail />,
-              },
-              {
-                text: "About",
-                link: "https://www.indiaspend.com/about-us",
-                icon: <GoInfo />,
-              },
-            ].map((item, index) => (
+        <div class="listbox">
+          {/* <div className="sidebar-content"> */}
+          <div className="sidebar_block">
+            {/* Sidebar Toggle Button */}
+            <p className="toggle-button sidebar-icon" onClick={toggleSidebar}>
+              {isCollapsed ? <GoSidebarCollapse /> : <GoSidebarExpand />}
+              <span className={`sidebar-text ${isCollapsed ? "" : "hidden"}`}>
+                Collapse
+              </span>
+            </p>
+
+            <p className="new-thread-button" onClick={handleNewThread}>
+              <span className="sidebar-icon">
+                <GoCrossReference />
+              </span>
+              <span className={`sidebar-text ${isCollapsed ? "" : "hidden"}`}>
+                Start new thread
+              </span>
+            </p>
+          </div>
+          {/* Quick Links */}
+          <div className="sidebar_block">
+            <div className={`ttlquicklink ${isCollapsed ? "" : "hidden"}`}>
+              {isCollapsed && <h5> Quick Links</h5>}
+
+              {[
+                {
+                  text: "IndiaSpend",
+                  link: "https://www.indiaspend.com",
+                  icon: <GoLink />,
+                },
+                {
+                  text: "Earthcheck India",
+                  link: "https://www.indiaspend.com/earthcheckindia",
+                  icon: <GoGlobe />,
+                },
+                {
+                  text: "Education Check",
+                  link: "https://www.indiaspend.com/education-check",
+                  icon: <GoRepo />,
+                },
+                {
+                  text: "GenderCheck",
+                  link: "https://www.indiaspend.com/gendercheck",
+                  icon: <BsGenderTrans />,
+                },
+                {
+                  text: "Newsletters",
+                  link: "https://www.indiaspend.com/subscribe",
+                  icon: <GoMail />,
+                },
+                {
+                  text: "About",
+                  link: "https://www.indiaspend.com/about-us",
+                  icon: <GoInfo />,
+                },
+              ].map((item, index) => (
+                <p
+                  key={index}
+                  className="faq-button"
+                  onClick={() => {
+                    window.open(item.link);
+                    setIsMobileMenuOpen(false);
+                  }}
+                >
+                  <span className="sidebar-icon">{item.icon}</span>
+                  <span
+                    className={`sidebar-text ${isCollapsed ? "" : "hidden"}`}
+                  >
+                    {item.text}
+                  </span>
+                </p>
+              ))}
+            </div>
+          </div>
+
+          <div className="sidebar_block">
+            <div className={`ttlquicklink ${isCollapsed ? "" : "hidden"}`}>
+              {isCollapsed && <h5>Title 2</h5>}
               <p
-                key={index}
                 className="faq-button"
-                onClick={() => window.open(item.link)}
+                onClick={() => {
+                  setShowFAQ(true);
+                  setShowFeedback(false);
+                  setIsMobileMenuOpen(false);
+                }}
               >
-                <span className="sidebar-icon">{item.icon}</span>
+                <span className="sidebar-icon">
+                  <GoQuestion />
+                </span>
                 <span className={`sidebar-text ${isCollapsed ? "" : "hidden"}`}>
-                  {item.text}
+                  FAQ
                 </span>
               </p>
-            ))}
+
+              {/* Feedback Button */}
+              <p
+                className="faq-button faq-text"
+                onClick={() => {
+                  setShowFeedback(true);
+                  setShowFAQ(false);
+                  setIsMobileMenuOpen(false);
+                }}
+              >
+                <span className="sidebar-icon Feedback-ur-text">
+                  <GoComment />
+                </span>
+                <span className={`sidebar-text ${isCollapsed ? "" : "hidden"}`}>
+                  Feedback
+                </span>
+              </p>
+            </div>
           </div>
-        </div>
 
-        <div className="sidebar_block">
-          <div className={`ttlquicklink ${isCollapsed ? "" : "hidden"}`}>
-            {isCollapsed && <h5>Title 2</h5>}
-            <p
-              className="faq-button"
-              onClick={() => {
-                setShowFAQ(true);
-                setShowFeedback(false);
-              }}
-            >
-              <span className="sidebar-icon">
-                <GoQuestion />
-              </span>
-              <span className={`sidebar-text ${isCollapsed ? "" : "hidden"}`}>
-                FAQ
-              </span>
-            </p>
-
-            {/* Feedback Button */}
-            <p
-              className="faq-button faq-text"
-              onClick={() => {
-                setShowFeedback(true);
-                setShowFAQ(false);
-              }}
-            >
-              <span className="sidebar-icon Feedback-ur-text">
-                <GoComment />
-              </span>
-              <span className={`sidebar-text ${isCollapsed ? "" : "hidden"}`}>
-                Feedback
-              </span>
-            </p>
-          </div>
-        </div>
-
-        {/* ✅ Start New Thread Button */}
-        {/* <p className="new-thread-button" onClick={handleNewThread}>
+          {/* ✅ Start New Thread Button */}
+          {/* <p className="new-thread-button" onClick={handleNewThread}>
           <span className="sidebar-icon">
             <GoCrossReference />
           </span>
@@ -187,30 +221,31 @@ function Sidebar({
           </span>
         </p> */}
 
-        {/* ✅ Previous Questions - Click karne par Trending Questions dikhai de */}
-        {isCollapsed && (
-          <div className="sidebar_block">
-            <div className="previous-questions">
-              <h5>Previous 7 Days</h5>
-              {previousQuestions.length > 0 ? (
-                <ul>
-                  {previousQuestions.map((item, index) => (
-                    <li
-                      key={index}
-                      onClick={() => setShowTrendingQuestions(true)}
-                    >
-                      <p>{item.question}</p>
-                    </li>
-                  ))}
-                </ul>
-              ) : (
-                <h5>No previous questions available</h5>
-              )}
+          {/* ✅ Previous Questions - Click karne par Trending Questions dikhai de */}
+          {isCollapsed && (
+            <div className="sidebar_block">
+              <div className="previous-questions">
+                <h5>Previous 7 Days</h5>
+                {previousQuestions.length > 0 ? (
+                  <ul>
+                    {previousQuestions.map((item, index) => (
+                      <li
+                        key={index}
+                        onClick={() => setShowTrendingQuestions(true)}
+                      >
+                        <p>{item.question}</p>
+                      </li>
+                    ))}
+                  </ul>
+                ) : (
+                  <h5>No previous questions available</h5>
+                )}
+              </div>
             </div>
-          </div>
-        )}
+          )}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
